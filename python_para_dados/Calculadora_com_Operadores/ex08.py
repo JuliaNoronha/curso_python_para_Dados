bcdeb8e5212cac9f7d7bd149e2a63dc723d0c0b9
Julia Noronha <devjulianoronha@gmail.com>
2026-08-03 15:17:55 -0300

    add: operadores ex08

---
+++ python_para_dados/Calculadora_com_Operadores/ex08.py
@@ -0,0 +1,6 @@
+numerador = int(input("Digite um número: "))
+denominador = int(input("Digite um número(não pode ser zero): "))
+
+resto = numerador % denominador
+
+print(f"O valor do resto é {resto}.")
