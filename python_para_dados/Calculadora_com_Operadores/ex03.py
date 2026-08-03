77082ed142d5cc460eb8b3751bea18f3e4c46195
Julia Noronha <devjulianoronha@gmail.com>
2026-08-03 14:57:20 -0300

    fix: operadores ex02 abs

--- python_para_dados/Calculadora_com_Operadores/ex03.py
+++ python_para_dados/Calculadora_com_Operadores/ex03.py
@@ -1,4 +1,4 @@
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
-subtracao = num1 - num2
+subtracao = abs(num1 - num2)
 print(f'A subtração dos valores é {subtracao}.')
