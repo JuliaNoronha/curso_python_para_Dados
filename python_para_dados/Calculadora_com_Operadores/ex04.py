4b981e8d581f55f27b55b6655018a7411e4868b9
Julia Noronha <devjulianoronha@gmail.com>
2026-08-03 15:01:37 -0300

    add: operadores ex04

---
+++ python_para_dados/Calculadora_com_Operadores/ex04.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+multiplicacao = num1 * num2
+
+print(f"O valor da multiplicação é de {multiplicacao}.")
