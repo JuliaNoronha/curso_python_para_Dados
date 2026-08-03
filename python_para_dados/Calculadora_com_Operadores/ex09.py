a2ed1ed5ed9abb7f30062ecbc04aef50315f8c16
Julia Noronha <devjulianoronha@gmail.com>
2026-08-03 15:21:11 -0300

    add: operadores ex09

---
+++ python_para_dados/Calculadora_com_Operadores/ex09.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Qual a nota da prova 1? "))
+nota2 = float(input("Qual a nota da prova 2? "))
+nota3 = float(input("Qual a nota da prova 3? "))
+
+media_notas = (nota1 + nota2 + nota3)/3
+print(f"Sua média é {media_notas}.")
