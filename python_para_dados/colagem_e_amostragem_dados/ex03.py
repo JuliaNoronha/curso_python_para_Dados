3946b5c83be925c3aaa99faf66def57774d9b15d
Julia Noronha <devjulianoronha@gmail.com>
2026-08-03 14:48:13 -0300

    add: primeiros passos ex03

---
+++ python_para_dados/colagem_e_amostragem_dados/ex03.py
@@ -0,0 +1,5 @@
+nome = input("Qual seu nome?")
+idade = int(input("Qual sua idade?"))
+altura = float(input("Qual sua altura em metros?"))
+
+print(f'Olá, {nome}, você tem {idade} anos e mede {altura} metros!')
