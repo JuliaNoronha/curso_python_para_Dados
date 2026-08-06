7104097ae95ebd49d5b26987579428ab9ea3150d
Julia Noronha <devjulianoronha@gmail.com>
2026-08-06 18:57:26 -0300

    fix[aula 3]: ex02

--- python_para_dados/aula 3/ex02.py
+++ python_para_dados/aula 3/ex02.py
@@ -1,6 +1,8 @@
 percentual = float(input("Digite o percentual de crescimento: "))
 
-if(percentual >= 0):
+if percentual > 0:
     print("Houve crescimento")
-else:
+elif percentual < 0:
     print("Houve um decrescimento")
+else:
+    print("Não houve mudança.")
