0c10f668e1bd0273806058a7ad2baaef7a3bc8a4
Julia Noronha <devjulianoronha@gmail.com>
2026-08-20 17:04:34 -0300

    add[aula 4]: ex03

--- python_para_dados/aula 4/ex03.py
+++ python_para_dados/aula 4/ex03.py
@@ -1,4 +1,4 @@
 for avaliacao in range(15):
     nota = int(input("Digite sua nota de 0 a 5: "))
     while nota < 0 or nota > 5:
-        nota = int(input("Digite sua nota de 0 a 5: "))
+        nota = int(input("Digite a nota novamente de 0 a 5: "))
